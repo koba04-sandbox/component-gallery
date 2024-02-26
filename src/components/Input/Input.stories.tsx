230f9b1132fa1a6ef5c8774ad5fa6efdfdd4e921
Toru Kobayashi <koba0004@gmail.com>
2024-02-27 00:20:19 +0900

    use @storybook/test instead of @storybook/testing-library|jest

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { userEvent, within } from "@storybook/testing-library";
-import { expect } from "@storybook/jest";
+import { userEvent, within, expect } from "@storybook/test";
 
 import { Input } from './index'
 
